e31dea5fc6e5e5033078fbb716ec0f222f8e3241
gramsower <110431307+gramsower@users.noreply.github.com>
2023-03-24 15:45:08 -0700

    add :bento: initial code to bean.js

---
+++ src/components/Bean.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Bean(props) {
+  return (
+    <React.Fragment>
+      <div onCLick = {() => props.whenBeanClicked(props.id)}>
+        <h3>{props.name}</h3>
+        <h4>{props.price}</h4>
+        <h4>{props.roast}</h4>
+        <h4>{props.origin}</h4>
+        <p>{props.tastingNotes}</p>
+        {/* Insert ability to track lbs remaining here */}
+        <hr />
+      </div>
+    </React.Fragment>
+  );
+}
+
+Bean.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  roast: PropTypes.string.isRequired,
+  origin: PropTypes.string.isRequired,
+  tastingNotes: PropTypes.string,
+  id: PropTypes.string,
+  whenBeanClicked: PropTypes.func
+};
