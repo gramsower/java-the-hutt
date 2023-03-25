126a44dc80942cf0a31177c74c9e3d010f21f1a1
gramsower <110431307+gramsower@users.noreply.github.com>
2023-03-25 13:40:10 -0700

    add code to display control

--- src/components/DisplayControl.js
+++ src/components/DisplayControl.js
@@ -10,27 +10,84 @@ class DisplayControl extends React.Component {
     super(props)
     this.state = {
       formVisibleOnPage: false,
-      mainBeanList = [],
+      mainBeanList: [],
       selectedBean: null,
       editing: false
     };
   }
 
+  handleClick = () => {
+    if (this.state.selectedBean !=null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedBean: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
+  }
+
+  handleAddingNewBeanToList = (newBean) => {
+    const newMainBeanList = this.state.mainBeanList.concat(newBean);
+    this.setState({mainBeanList: newMainBeanList,
+                  formVisibleOnPage: false })
+    }
+
+  handleChangingSelectedBean = (id) => {
+    const selectedBean = this.state.mainBeanList.filter(bean => bean.id === id)[0];
+    this.setState({selectedBean: selectedBean});
+  }
 
 
+  handleEditClick = () => {
+    this.setState({editing: true });
+  }
 
+  handleEditingBeanInList = (beanToEdit) => {
+    const editedMainBeanList = this.state.mainBeanList
+    .filter(bean => bean.id !==this.state.selectedBean.id)
+    .concact.apply(beanToEdit);
+    this.setState({
+      mainBeanList: editedMainBeanList,
+      editing: false,
+      selectedBean: null
+    });
+  }
 
-
-
-
-
-
-
-
-
+  handleDeletingBean = (id) => {
+    const newMainBeanList = this.state.mainBeanList.filter(bean => bean.id !== id);
+    this.setState({
+      mainBeanList: newMainBeanList,
+      selectedBean: null
+    });
+  }
 
   render () {
     let currentlyVisibleState = null;
     let buttonText = null;
 
-  }
+    if(this.state.editing) {
+      currentlyVisibleState = <EditBeanForm bean = { this.state.selectedBean } onEditBean = {this.handleEditingBeanInList } />
+      buttonText= "Return to Bean List";
+    } else if(this.state.selectedBean !=null) {
+      currentlyVisibleState = <BeanDetail
+                                bean = { this.state.selectedBean }
+                                onClickingDelete = { this.handleDeletingBean }
+                                onClickingEdit = { this.handleEditClick } />
+      buttonText= "Return to Bean List";
+    } else {
+      currentlyVisibleState = <NewBeanForm onNewBeanCreation={this.handleAddingNewBeanToList} />;
+      buttonText="Add Beans"
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+          <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
+  }  
+}
+export default DisplayControl;
