c412baae0d51c5bb74db9ed4eaf82ee1d44f2117
gramsower <110431307+gramsower@users.noreply.github.com>
2023-03-24 18:22:07 -0700

    DRY up code with reusable form

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -4,5 +4,38 @@ import PropTypes from "prop-types";
 function ReusableForm(props) {
   return (
     <React.Fragment>
-      
+      <form onSubmit = {props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Bean Name' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price per pound (USD)' />
+        <input 
+          type='text'
+          name='roast'
+          placeholder='Roast' />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Place of Origin' />
+        <textarea
+          name='tastingNotes'
+          placeholder='Tasting Notes' />
+        <input
+          type='number'
+          name='amount'
+          placeholder='Amount Available' />
+      </form>
     </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
